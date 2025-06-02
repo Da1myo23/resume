f716d26dc22fabd58453b2bead17e2c97dd9622a
Da1myo23 <miloserdowil@yandex.ru>
2025-06-02 18:17:16 +0300

    op

---
+++ Debug2/index.js
@@ -0,0 +1,10 @@
+const temperatureInCelsius = prompt('Введите температуру в градусах Цельсия');
+
+if (Number(temperatureInCelsius) <= 0) {
+   alert('0 градусов по Цельсию - это температура замерзания воды')
+} else {
+   alert('Для замерзания воды температура должна быть 0 градусов по Цельсию либо ниже');
+};
+
+const temperatureInFahrenheit = temperatureInCelsius * 9 / 5 + 32;
+alert(`${temperatureInCelsius} градусов по Цельсию - это ${temperatureInFahrenheit} по Фаренгейту.`);
